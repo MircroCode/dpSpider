92e8bbb945ce780d3ae2843906381057c3b5b70f
youjiezeng <youjiezeng@sohu-inc.com>
2015-01-20 18:02:49 +0800

    BaseSpider

--- dianping/dianping/spiders/dpspider.py
+++ dianping/dianping/spiders/dpspider.py
@@ -33,44 +33,6 @@ scrapy runspider my_spider.py
 '''
 
 
-class RotateUserAgentMiddleware(UserAgentMiddleware):
-    def __init__(self, user_agent=''):
-        self.user_agent = user_agent
-
-    def process_request(self, request, spider):
-        #这句话用于随机选择user-agent
-        ua = random.choice(self.user_agent_list)
-        if ua:
-            request.headers.setdefault('User-Agent', ua)
-            print '********user-agent:',ua
-
-    #the default user_agent_list composes chrome,I E,firefox,Mozilla,opera,netscape
-    #for more user agent strings,you can find it in http://www.useragentstring.com/pages/useragentstring.php
-    user_agent_list = [\
-        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1"\
-        "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",\
-        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",\
-        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",\
-        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",\
-        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",\
-        "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",\
-        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",\
-        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",\
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",\
-        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",\
-        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",\
-        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",\
-        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",\
-        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",\
-        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",\
-        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",\
-        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
-       ]
-
-
-
-
-
 '''
 It’s a derived class of BaseSpider which has three fields and one method (all are required):
 
@@ -85,9 +47,7 @@ class DpSpider(BaseSpider):
     allowed_domains = ['dianping.com']
     start_urls = ['http://www.dianping.com/search/category/2/20/g120r1481']
     # rules = [Rule(LxmlParserLinkExtractor(['shop/\d+']), 'parse')]
-    # rules = [Rule(SgmlLinkExtractor(allow=('/shop'),
-    #                                         restrict_xpaths=('//@href')), callback='parse',
-    #                       follow=True)]
+    # rules = [Rule(SgmlLinkExtractor(allow=('/shop'),restrict_xpaths=('//@href')), callback='parse',follow=True)]
 
     def parse(self, response):
 
@@ -140,15 +100,15 @@ class DpSpider(BaseSpider):
         # urls = htmlData.select('//@href').extract()
         urlPattern = re.compile('shop/[0-9]+')
 
-        # for url in urls:
-        #     if not urlPattern.findall(url):
-        #         continue
-        #     if str(url.encode('utf8')).startswith('http'):
-        #         pass
-        #     elif str(url.encode('utf8')).startswith('/'):
-        #         url = domainUrl+url
-        #     else:
-        #         url = currentUrl + url
-        #     print '------------url:',url
-        #     yield Request(url,callback=self.parse)
+        for url in urls:
+            if not urlPattern.findall(url):
+                continue
+            if str(url.encode('utf8')).startswith('http'):
+                pass
+            elif str(url.encode('utf8')).startswith('/'):
+                url = domainUrl+url
+            else:
+                url = currentUrl + url
+            print '------------url:',url
+            yield Request(url,callback=self.parse)
 
